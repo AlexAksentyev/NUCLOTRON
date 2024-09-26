5fa4f65baa94b05525978d855b8e72b568f48fe4
Alex Aksentyev <alexaksentyev@gmail.com>
2024-09-26 15:51:41 +0300

    minor script changes in test_tss.*

--- analysis/test_tss.py
+++ analysis/test_tss.py
@@ -20,9 +20,9 @@ mrkr_form = lambda n: 'CASE_{:d}'.format(n)
 case_sign = '*'
 
 def load_tss(dir):
-    cases = [int(re.findall(r'\d+',e)[1]) for e in glob(DATDIR+'ABERRATIONS:CW_'+case_sign)]
+    cases = [int(re.findall(r'\d+',e)[1]) for e in glob(dir+'ABERRATIONS:CW_'+case_sign)]
     cases.sort()
-    cases=np.arange(10)
+    cases=np.arange(51)
     ncases = len(cases)
     nbar = {}; nu = {}
     n0 = np.zeros(ncases, dtype=list(zip(['X_CW','Y_CW','Z_CW','X_CCW','Y_CCW','Z_CCW','tilt'],[float]*7)));
@@ -32,11 +32,11 @@ def load_tss(dir):
         print(case)
         tmp = []
         for dn in ['CW','CCW']:
-            nbar.update({str(case)+dn: NBAR(DATDIR, dn+'_'+mrkr_form(case))})
-            nu.update({str(case)+dn: DAVEC(DATDIR+'MU:'+dn+'_'+mrkr_form(case)+'.da')})
+            nbar.update({str(case)+dn: NBAR(dir, dn+'_'+mrkr_form(case))})
+            nu.update({str(case)+dn: DAVEC(dir+'MU:'+dn+'_'+mrkr_form(case)+'.da')})
             tmp += [nbar[str(case)+dn].mean[e] for e in range(3)]
             
-        tilts[i] = np.loadtxt(DATDIR+'TILTS:'+mrkr_form(case)+'.in')
+        tilts[i] = np.loadtxt(dir+'TILTS:'+mrkr_form(case)+'.in')
         n0[i] = tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5], tilts[i].mean()
         nu0[i] = nu[str(case)+'CW'].const, nu[str(case)+'CCW'].const
     return nu, nbar, nu0, n0, tilts
